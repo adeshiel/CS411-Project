a731d78336c862050e3b23defa8883511e008acb
Ashley Deshields <adeshiel@bu.edu>
2019-03-28 12:13:47 -0400

    changes for lab

--- lab3.py
+++ lab3.py
@@ -10,6 +10,11 @@ from KEYS import CF_KEY
 
 app = Flask(__name__)
 
+if (__name__ == '__main__'):
+   app.run(
+       debug=True,
+   )
+
 CF.Key.set(CF_KEY)
 
 BASE_URL = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
@@ -23,7 +28,9 @@ def homepage():
 
 @app.route("/return", methods=['GET', 'POST'])
 def retpage():
-    return render_template("return.html")
+    video= request.form['video']
+    analysis = forLab(video)
+    return render_template("index.html", video=analysis)
 
 def findEmotions(vid, turns):
     assert turns >= 0, "Cannot have negative turn value"
@@ -86,16 +93,62 @@ def findEmotions(vid, turns):
     cv2.destroyAllWindows()
 
     for key in TotalEmotionAverage:
-        TotalEmotionAverage[key] = Average(TotalEmotionAverage[key]) 
+        TotalEmotionAverage[key] = Average(TotalEmotionAverage[key])
 
     highestAvgEmotion =  max(TotalEmotionAverage, key=TotalEmotionAverage.get)
     print('The overall emotion of people in this video is: ' + highestAvgEmotion)
 
+def forLab(vid):
+        cap = cv2.VideoCapture(vid)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            cv2.imwrite("current.png", frame)
 
+            result = CF.face.detect("current.png", attributes='emotion')
+
+            try:
+                if result == []:
+                    continue
+
+                elif len(result) > 1:
+                    emoList = []
+
+                    for face in result:
+                        emotes = face['faceAttributes']['emotion']
+                        print(emotes)
+                        emoList.append(emotes)
+
+                        # TODO: if there is more than one face, average out emotion
+                        # for now we'll take the first one
+
+                    emo = emoList[0]
+                elif len(result) == 1:
+                    emo = result[0]['faceAttributes']['emotion']
+                    print(emo)
+
+                ## this block shows a separate window with live frames and edits
+                ## Use it for testing!
+                # cv2.putText(frame, str(highestEmotion), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+                # cv2.imshow('Test', frame)
+                # cv2.waitKey(2)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                break
+
+            except KeyboardInterrupt:
+                cap.release()
+                cv2.destroyAllWindows()
+
+        ## and when finished
+        cap.release()
+        cv2.destroyAllWindows()
+
+        return emo
 
 
 def Average(lst):
     return sum(lst) / len(lst)
 
 
-findEmotions('caolanTest.mp4', 60)
+# findEmotions('caolanTest.mp4', 60)
